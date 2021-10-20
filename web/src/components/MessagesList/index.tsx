37518f86653b8fbaef3d74d0ffcc00c156ceb6e7
RennanD <rplaykl@gmail.com>
2021-10-19 21:53:39 -0300

    web | chore: create Messages List

--- web/src/components/MessagesList/index.tsx
+++ web/src/components/MessagesList/index.tsx
@@ -2,6 +2,54 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
+import logoImage from '../../assets/logo.svg';
+
 export function MessagesList(): JSX.Element {
-  return <div />;
+  return (
+    <div className={styles.messagesListWrapper}>
+      <img src={logoImage} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento,
+            com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/rennand.png" alt="Rennan Douglas" />
+            </div>
+            <span>Rennan Douglas</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento,
+            com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/rennand.png" alt="Rennan Douglas" />
+            </div>
+            <span>Rennan Douglas</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento,
+            com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/rennand.png" alt="Rennan Douglas" />
+            </div>
+            <span>Rennan Douglas</span>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  );
 }
